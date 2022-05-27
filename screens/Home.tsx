74c594d635148c8f379c2158c1aa9fc36a637356
mikayilsrt <sert.mikayil@gmail.com>
2022-05-27 19:21:01 +0200

    feat: add card sections

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -11,10 +11,53 @@ const HomeContainer = styled(Container)`
     flex: 1;
 `;
 
+import logo1 from "./../assets/cards/mc.png"
+import logo2 from "./../assets/cards/visa_white.png"
+import CardSection from "../components/Cards/CardSection";
+
 const Home: FunctionComponent = () => {
+    const cardsData = [
+        {
+            id: 1,
+            accountNo: "4252425412341341",
+            balance: 20000.15,
+            alias: "Work Debit",
+            logo: logo1,
+        },
+        {
+            id: 2,
+            accountNo: "4252425412341341",
+            balance: 20000.15,
+            alias: "Work Debit",
+            logo: logo2,
+        },
+        {
+            id: 3,
+            accountNo: "4252425412341341",
+            balance: 20000.15,
+            alias: "Work Debit",
+            logo: logo1,
+        },
+        {
+            id: 4,
+            accountNo: "4252425412341341",
+            balance: 20000.15,
+            alias: "Work Debit",
+            logo: logo2,
+        },
+        {
+            id: 5,
+            accountNo: "4252425412341341",
+            balance: 20000.15,
+            alias: "Work Debit",
+            logo: logo1,
+        },
+    ];
+
     return (
         <HomeContainer>
             <StatusBar style="dark" />
+            <CardSection data={cardsData} />
         </HomeContainer>
     );
 };
